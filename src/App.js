712e7b679e411c8d969f6a8f9ed1abc0d08c293e
machineawin <machineawin@gmail.com>
2022-03-09 21:42:59 +1100

    Add inline style using double curly braces

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Components/Header';
 function App() {
   return (
     <div className="container">
-      <Header title = 'Not a number'/>
+      <Header title = 'Passed in title'/>
     </div>
   );
   
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 const Header = (props) => {
   return (
     <header>
-        <h1>{props.title}</h1>        
+        <h1 style={{color: 'red', backgroundColor: 'blue'}}>{props.title}</h1>        
     </header>
 
   )
 }
 
+
 export default Header
 
 Header.defaultProps = {
   title: 'Default Title',
-  age: '23',
+  age: 23,
 };
 
 Header.propTypes = {
-  title: PropTypes.number,
-  age: PropTypes.string,
+  title: PropTypes.string,
+  age: PropTypes.number,
 };
