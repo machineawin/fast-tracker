3aafd47aa5085361c2b20d21f4eda824f5296459
machineawin <machineawin@gmail.com>
2022-03-09 21:21:15 +1100

    Implement defaultProps

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-        <h1>Fast Tracker</h1>
+        <h1>{props.title}{props.age}</h1>        
     </header>
 
   )
 }
 
 export default Header
+
+Header.defaultProps = {
+  title: 'Default Title',
+  age: '23',
+};
+
