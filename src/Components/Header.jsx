4e8e7a2c6edb23343a73e6052180009d162c8749
machineawin <machineawin@gmail.com>
2022-03-09 21:44:05 +1100

    Add styling using object with styling info

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 const Header = (props) => {
   return (
     <header>
-        <h1 style={{color: 'red', backgroundColor: 'blue'}}>{props.title}</h1>        
+        <h1 style={myStyle}>{props.title}</h1>        
     </header>
 
   )
 }
 
+const myStyle = {
+  color: 'red',
+  backgroundColor: 'lightblue'
+};
 
 export default Header
 
