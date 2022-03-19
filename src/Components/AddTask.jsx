f19bce1c17918b17e0f6090ff4bab70706423140
machineawin <machineawin@gmail.com>
2022-03-19 16:51:23 +1100

    Create state for AddTask

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -1,24 +1,43 @@
+import { useState } from "react"
+
 const AddTask = () => {
-  return (
-    <form className="add-form">
-        <div className="form-control">
-            <label>Task</label>
-            <input type = 'text' placeholder="Add Task"></input>
-        </div>
-        <div className="form-control">
-            <label>Time</label>
-            <input type = 'text' placeholder="Add Day and Time"></input>
-        </div>
-        <div className="form-control form-control-check">
-            <label>Set Reminder</label>
-            <input type = 'checkbox'></input>
-        </div>
+    const [task, setTask] = useState ('')
+    const [time, setTime] = useState ('')
+    const [reminder, setReminder] = useState (false)
 
-        <input type='submit' value='Save Task' className="btn btn-block"/>
+    return (
+        <form className="add-form">
+            <div className="form-control">
+                <label>Task</label>
+                <input type = 'text' 
+                placeholder="Add Task" 
+                value={task} 
+                onChange = {(e) => setTask (e.target.value)}
+                ></input>
+            </div>
+            <div className="form-control">
+                <label>Time</label>
+                <input 
+                type = 'text' 
+                placeholder="Add Day and Time"
+                value={time} 
+                onChange = {(e) => setTime (e.target.value)}
+                ></input>
+            </div>
+            <div className="form-control form-control-check">
+                <label>Set Reminder</label>
+                <input 
+                type = 'checkbox'
+                value={reminder} 
+                onChange = {(e) => setReminder (e.currentTarget.checked)}
+                ></input>
+            </div>
+
+            <input type='submit' value='Save Task' className="btn btn-block"/>
 
 
-    </form>
-  )
+        </form>
+    )
 }
 
 export default AddTask
